20b327ced7b8000e5a12a56e2006aad77d5b3d57
CaraRota <canalsdigital@gmail.com>
2024-02-14 22:39:14 -0300

    Refactor validateEmail.js excercise

--- validateEmail.js
+++ validateEmail.js
@@ -21,40 +21,58 @@ Your function should perform basic validation checks according to the criteria m
 
 Write the isValidEmail function and test it with different email addresses to ensure it works correctly.
 */
-const email = "user@example.com";
+const email = "user@example";
 
 const isValidEmail = (email) => {
-    let arrobaValidation = false;
-    let secondPartValidation = false;
+    const splitEmail = email.split("@");
 
-    //Check if email includes @, and if @ is not the first or last character
-    if (email[0] === "@" || email[email.length - 1] === "@") {
+    if (splitEmail.length !== 2 || splitEmail[0].length < 1 || splitEmail[1].length < 1) {
         return false;
     }
 
-    //Check if before and after @ there are characters
-    for (let i = 0; i < email.length; i++) {
-        if (email[i] === "@") {
-            if (email[i - 1] && email[i + 1] && email[i + 1] !== ".") {
-                arrobaValidation = true;
-            }
-        }
+    const splitDot = splitEmail[1].split(".");
+
+    if (splitDot.length < 2 || splitDot[0].length < 1 || splitDot[1].length < 1) {
+        return false;
     }
 
-    const split = email.split("@");
-    const secondPart = split[1];
-
-    if (secondPart.includes(".")) {
-        let afterDot = secondPart.split(".");
-        if (afterDot[1].length > 0) {
-            secondPartValidation = true;
-        }
-    }
-
-    if (arrobaValidation && secondPartValidation) {
-        return true;
-    }
-    return false;
+    return true;
 };
 
 console.log(isValidEmail(email));
+
+// const isValidEmail = (email) => {
+//     let arrobaValidation = false;
+//     let secondPartValidation = false;
+
+//     //Check if email includes @, and if @ is not the first or last character
+//     if (email[0] === "@" || email[email.length - 1] === "@") {
+//         return false;
+//     }
+
+//     //Check if before and after @ there are characters
+//     for (let i = 0; i < email.length; i++) {
+//         if (email[i] === "@") {
+//             if (email[i - 1] && email[i + 1] && email[i + 1] !== ".") {
+//                 arrobaValidation = true;
+//             }
+//         }
+//     }
+
+//     const split = email.split("@");
+//     const secondPart = split[1];
+
+//     if (secondPart.includes(".")) {
+//         let afterDot = secondPart.split(".");
+//         if (afterDot[1].length > 0) {
+//             secondPartValidation = true;
+//         }
+//     }
+
+//     if (arrobaValidation && secondPartValidation) {
+//         return true;
+//     }
+//     return false;
+// };
+
+// console.log(isValidEmail(email));
